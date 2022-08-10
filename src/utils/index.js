eff19592d4077d171fe9ffe4b891dd5ddffc61b3
zhengshugan <zhengshugan@foxmail.com>
2022-08-10 15:08:47 +0800

    feat: add util module

---
+++ src/utils/index.js
@@ -0,0 +1,43 @@
+import { request } from './request'
+
+const loadJS = (url) => {
+	return new Promise((resolve, reject) => {
+		const script = document.createElement('script')
+		script.src = url
+		script.type = 'text/javascript'
+		document.body.appendChild(script)
+		script.onload = () => {
+			resolve()
+		}
+	})
+}
+
+const loadCSS = (url) => {
+	return new Promise((resolve, reject) => {
+		const link = document.createElement('link')
+		link.rel = 'stylesheet'
+		link.href = url
+		document.head.appendChild(link)
+		link.onload = () => {
+			resolve()
+		}
+	})
+}
+
+const generateUUID = () => {
+	let d = new Date().getTime()
+	let uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+		let r = (d + Math.random() * 16) % 16 | 0
+		d = Math.floor(d / 16)
+		return (c === 'x' ? r : (r & 0x7 | 0x8).toString(16))
+	})
+
+	return uuid
+}
+
+export {
+	generateUUID,
+	loadCSS,
+	loadJS,
+	request
+}
